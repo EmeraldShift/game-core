ce16b00e0a34c778eecfa68ea8324db4ca465756
Michael Jarrett <admin@lithiagaming.com>
2015-12-07 10:20:42 -0600

    Add basic JFrame functionality

--- game-core/src/com/lithia/game/Game.java
+++ game-core/src/com/lithia/game/Game.java
@@ -1,6 +1,64 @@
 package com.lithia.game;
 
-public class Game
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable
 {
-	;
+	
+	public static final String NAME = "Game";
+	public static final int WIDTH = 240;
+	public static final int HEIGHT = 160;
+	public static final int SCALE = 3;
+	
+	private boolean running;
+
+	private void init()
+	{
+		
+	}
+	
+	public void start()
+	{
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public void stop()
+	{
+		running = false;
+	}
+	
+	public void run()
+	{
+		init();
+		
+		while(running)
+		{
+			// TODO game loop
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Game game = new Game();
+		game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		
+		JFrame frame = new JFrame(NAME);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(game, BorderLayout.CENTER);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		game.start();
+	}
+	
 }
